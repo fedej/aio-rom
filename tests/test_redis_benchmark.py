0e2394cb2cb344ce09ad15762fd10d50206af56a
Federico Jaite <fede_654_87@hotmail.com>
2022-08-04 21:59:40 +0100

    Switch tests to pytest

--- tests/test_redis_benchmark.py
+++ tests/test_redis_benchmark.py
@@ -3,9 +3,8 @@ from __future__ import annotations
 import asyncio
 import dataclasses
 import os
+import typing
 from dataclasses import field
-from typing import Any, Awaitable, Callable
-from unittest import TestCase, skipUnless
 
 import pytest
 from pytest_benchmark.fixture import BenchmarkFixture  # type: ignore[import]
@@ -15,6 +14,9 @@ from aio_rom import DataclassModel as Model
 from aio_rom.collections import RedisList
 from aio_rom.fields import Metadata
 from aio_rom.session import connection
+
+if not os.environ.get("CI"):
+    pytest.skip("Redis benchmark CI test only", allow_module_level=True)
 
 
 @dataclasses.dataclass
@@ -31,79 +33,112 @@ class Foo(Model):
     )
 
 
-@skipUnless(os.environ.get("CI"), "Redis benchmark CI test only")
-class Benchmark(TestCase):
-    items = 100
+ITEMS = 100
 
-    @pytest.fixture(autouse=True)
-    def setupBenchmark(self, benchmark: BenchmarkFixture) -> None:
-        self.benchmark = benchmark
 
-    def run_coro(self, coro_factory: Callable[[], Awaitable[Any]]) -> Any:
-        return asyncio.run(coro_factory())
+def run_benchmark(
+    benchmark: BenchmarkFixture,
+    coro: typing.Callable[..., typing.Awaitable[None]],
+    event_loop: asyncio.AbstractEventLoop,
+) -> None:
+    def run() -> None:
+        event_loop.run_until_complete(coro())
 
-    async def delete_all(self) -> None:
-        await Bar.delete_all()
-        await Foo.delete_all()
+    benchmark(run)
 
-    def setUp(self) -> None:
-        self.bar = Bar("1", 123, "value", RedisList[int]([1, 2, 3]))
-        asyncio.run(self.bar.save())
 
-    def tearDown(self) -> None:
-        asyncio.run(self.delete_all())
+@pytest.fixture
+async def item() -> typing.AsyncIterator[Bar]:
+    item = Bar("1", 123, "value", RedisList[int]([1, 2, 3]))
+    await item.save()
+    yield item
+    await Bar.delete_all()
+    await Foo.delete_all()
 
-    def test_save(self) -> None:
-        async def save() -> None:
-            async with connection():
-                for _ in range(self.items):
-                    await self.bar.save()
 
-        self.benchmark(self.run_coro, save)
+@pytest.fixture
+async def item_list() -> typing.AsyncIterator[None]:
+    foo = Foo("1")
+    for i in range(ITEMS):
+        foo.bars.append(Bar(str(i), 123, "value", RedisList[int]([1, 2, 3])))
+    await foo.save()
+    yield
+    await Bar.delete_all()
+    await Foo.delete_all()
 
-    def test_get(self) -> None:
-        async def get() -> None:
-            async with connection():
-                for _ in range(self.items):
-                    await Bar.get("1")
 
-        self.benchmark(self.run_coro, get)
+@pytest.mark.benchmark
+def test_save(
+    item: Bar, event_loop: asyncio.AbstractEventLoop, benchmark: BenchmarkFixture
+) -> None:
+    async def do() -> None:
+        async with connection():
+            for _ in range(ITEMS):
+                await item.save()
 
-    def test_get_eager_list(self) -> None:
+    run_benchmark(benchmark, do, event_loop)
+
+
+@pytest.mark.benchmark
+def test_get(
+    item: Bar, event_loop: asyncio.AbstractEventLoop, benchmark: BenchmarkFixture
+) -> None:
+    async def do() -> None:
+        async with connection():
+            for _ in range(ITEMS):
+                await Bar.get("1")
+
+    run_benchmark(benchmark, do, event_loop)
+
+
+@pytest.mark.benchmark
+def test_get_eager_list(
+    item_list: typing.Any,
+    event_loop: asyncio.AbstractEventLoop,
+    benchmark: BenchmarkFixture,
+) -> None:
+    async def do() -> None:
+        async with connection():
+            foo = await Foo.get("1")
+        assert len(foo.bars) == ITEMS
+
+    run_benchmark(benchmark, do, event_loop)
+
+
+@pytest.mark.benchmark
+def test_get_all(
+    item_list: typing.Any,
+    benchmark: BenchmarkFixture,
+    event_loop: asyncio.AbstractEventLoop,
+) -> None:
+    async def do() -> None:
+        result = await Bar.all()
+        assert len(result) == ITEMS
+
+    run_benchmark(benchmark, do, event_loop)
+
+
+@pytest.mark.benchmark
+def test_scan_all(
+    item_list: typing.Any,
+    benchmark: BenchmarkFixture,
+    event_loop: asyncio.AbstractEventLoop,
+) -> None:
+    async def do() -> None:
+        result = [item async for item in Bar.scan()]
+        assert len(result) == ITEMS
+
+    run_benchmark(benchmark, do, event_loop)
+
+
+@pytest.mark.benchmark
+def test_cascade_save(
+    benchmark: BenchmarkFixture, event_loop: asyncio.AbstractEventLoop
+) -> None:
+    async def do() -> None:
         foo = Foo("1")
-        for i in range(self.items):
+        for i in range(ITEMS):
             foo.bars.append(Bar(str(i), 123, "value", RedisList[int]([1, 2, 3])))
-        asyncio.run(foo.save())
+        await foo.save()
 
-        async def get() -> Foo:
-            async with connection():
-                return await Foo.get("1")
-
-        result = self.benchmark(self.run_coro, get)
-        assert self.items == len(result.bars)
-
-    def test_get_all(self) -> None:
-        for i in range(self.items):
-            asyncio.run(Bar(str(i), 123, "value", RedisList[int]([1, 2, 3])).save())
-
-        result = self.benchmark(self.run_coro, Bar.all)
-        assert self.items == len(result)
-
-    def test_scan_all(self) -> None:
-        for i in range(self.items):
-            asyncio.run(Bar(str(i), 123, "value", RedisList[int]([1, 2, 3])).save())
-
-        async def scan() -> list[Bar]:
-            result = []
-            async for item in Bar.scan():
-                result.append(item)
-            return result
-
-        result = self.benchmark(self.run_coro, scan)
-        assert self.items == len(result)
-
-    def test_cascade_save(self) -> None:
-        foo = Foo("1")
-        for i in range(self.items):
-            foo.bars.append(Bar(str(i), 123, "value", RedisList[int]([1, 2, 3])))
-        self.benchmark(self.run_coro, foo.save)
+    run_benchmark(benchmark, do, event_loop)
--- tests/test_redis_collections.py
+++ tests/test_redis_collections.py
@@ -1,13 +1,15 @@
 import dataclasses
 import os
-from unittest import skipUnless
+
+import pytest
 
 from aio_rom import DataclassModel as Model
 from aio_rom.collections import RedisCollection, RedisList, RedisSet
 from aio_rom.exception import ModelNotFoundException
 from aio_rom.session import connection
 
-from . import RedisTestCase
+if not os.environ.get("CI"):
+    pytest.skip("Redis CI test only", allow_module_level=True)
 
 
 @dataclasses.dataclass(unsafe_hash=True)
@@ -15,155 +17,172 @@ class Foo(Model):
     field1: int
 
 
-@skipUnless(os.environ.get("CI"), "Redis CI test only")
-class RedisCollectionIntegrationTestCase(RedisTestCase):
-    async def asyncSetUp(self) -> None:
-        self.foo = Foo("1", 123)
+@pytest.fixture
+def foo() -> Foo:
+    return Foo("1", 123)
 
-    async def asyncTearDown(self) -> None:
-        async with connection() as conn:
-            await conn.flushdb()
 
-    async def _test_save_model_collection(
-        self, redis_collection: RedisCollection[Foo], cascade: bool = False
-    ) -> None:
-        assert redis_collection.id
-        await redis_collection.save(cascade=cascade)
-        if cascade:
-            for item in redis_collection:
-                assert await Foo.get(item.id) == item
-            fresh = await redis_collection.get(redis_collection.id)
-            await fresh.refresh()
-            assert redis_collection == fresh
-        else:
-            for item in redis_collection:
-                with self.assertRaises(ModelNotFoundException):
-                    await Foo.get(item.id)
+@pytest.fixture(autouse=True)
+async def cleanup() -> None:
+    async with connection() as conn:
+        await conn.flushdb()
 
-    async def test_save_model_list(self) -> None:
-        redis_list = RedisList[Foo]([self.foo], id="test")
-        await self._test_save_model_collection(redis_list)
 
-    async def test_save_model_list_cascade(self) -> None:
-        redis_list = RedisList[Foo]([self.foo], id="test")
-        await self._test_save_model_collection(redis_list, cascade=True)
+async def _test_save_model_collection(
+    redis_collection: RedisCollection[Foo], cascade: bool = False
+) -> None:
+    assert redis_collection.id
+    await redis_collection.save(cascade=cascade)
+    if cascade:
+        for item in redis_collection:
+            assert await Foo.get(item.id) == item
+        fresh = await redis_collection.get(redis_collection.id)
+        await fresh.refresh()
+        assert redis_collection == fresh
+    else:
+        for item in redis_collection:
+            with pytest.raises(ModelNotFoundException):
+                await Foo.get(item.id)
 
-    async def test_save_model_set(self) -> None:
-        redis_set = RedisSet[Foo]({self.foo}, id="test")
-        await self._test_save_model_collection(redis_set)
 
-    async def test_save_model_set_cascade(self) -> None:
-        redis_set = RedisSet[Foo]({self.foo}, id="test")
-        await self._test_save_model_collection(redis_set, cascade=True)
+async def test_save_model_list(foo: Foo) -> None:
+    redis_list = RedisList[Foo]([foo], id="test")
+    await _test_save_model_collection(redis_list)
 
-    async def test_save_redis_list(self) -> None:
-        redis_list = RedisList[int](id="int_list")
-        redis_list.append(1)
-        redis_list.append(2)
-        redis_list.append(3)
-        await redis_list.save()
 
-        async with connection() as redis:
-            assert ["1", "2", "3"] == await redis.lrange("redislist:int_list", 0, -1)
+async def test_save_model_list_cascade(foo: Foo) -> None:
+    redis_list = RedisList[Foo]([foo], id="test")
+    await _test_save_model_collection(redis_list, cascade=True)
 
-        int_list = await RedisList[int].get("int_list")
-        await int_list.refresh()
-        assert [1, 2, 3] == int_list.values
 
-    async def test_save_redis_set(self) -> None:
-        redis_set = RedisSet[str](id="some_set")
-        redis_set.add("test")
-        redis_set.add("ing")
-        await redis_set.save()
+async def test_save_model_set(foo: Foo) -> None:
+    redis_set = RedisSet[Foo]({foo}, id="test")
+    await _test_save_model_collection(redis_set)
 
-        async with connection() as redis:
-            assert {"test", "ing"} == await redis.smembers("redisset:some_set")
 
-        some_set = await RedisSet[str].get("some_set")
-        await some_set.refresh()
-        assert {"test", "ing"} == some_set.values
+async def test_save_model_set_cascade(foo: Foo) -> None:
+    redis_set = RedisSet[Foo]({foo}, id="test")
+    await _test_save_model_collection(redis_set, cascade=True)
 
-    async def test_iteration_set(self) -> None:
-        redis_set = RedisSet[str](id="some_set")
-        redis_set.add("test")
-        redis_set.add("ing")
-        await redis_set.save()
-        some_set = await RedisSet[str].get("some_set")
-        async for item in some_set:
-            assert item in redis_set
 
-    async def test_iteration_list(self) -> None:
-        redis_list = RedisList[str](id="some_list")
-        redis_list.append("test")
-        redis_list.append("ing")
-        await redis_list.save()
-        some_list = await RedisList[str].get("some_list")
-        async for item in some_list:
-            assert item in redis_list
+async def test_save_redis_list() -> None:
+    redis_list = RedisList[int](id="int_list")
+    redis_list.append(1)
+    redis_list.append(2)
+    redis_list.append(3)
+    await redis_list.save()
 
-    async def test_model_iteration(self) -> None:
-        model_list = RedisList[Foo]([self.foo], id="models")
-        await model_list.save(cascade=True)
-        models = await RedisList[Foo].get("models")
-        async for item in models:
-            assert item in model_list
-        assert [self.foo] == models
+    async with connection() as redis:
+        assert ["1", "2", "3"] == await redis.lrange("redislist:int_list", 0, -1)
 
-    async def test_async_set(self) -> None:
-        a_set = RedisSet[str](id="a_set")
-        await a_set.async_add("123")
-        await a_set.async_add("456")
-        assert len(a_set) == 2
-        await a_set.async_discard("456")
-        assert len(a_set) == 1
-        new_set = await RedisSet[str].get("a_set")
-        await new_set.refresh()
-        assert a_set == new_set
+    int_list = await RedisList[int].get("int_list")
+    await int_list.refresh()
+    assert [1, 2, 3] == int_list.values
 
-    async def test_async_list(self) -> None:
-        a_list = RedisList[str](id="a_list")
-        await a_list.async_append("123")
-        await a_list.async_append("456")
-        assert len(a_list) == 2
-        saved_list = await RedisList[str].get("a_list")
-        await saved_list.refresh()
-        assert saved_list == a_list
 
-    async def test_async_model_set(self) -> None:
-        a_set = RedisSet[Foo](id="a_set")
-        await a_set.async_add(Foo("123", 123), cascade=True)
-        await a_set.async_add(Foo("456", 456), cascade=True)
-        assert len(a_set) == 2
-        await a_set.async_discard(Foo("456", 456), cascade=True)
-        assert len(a_set) == 1
-        saved_set = await RedisSet[Foo].get("a_set")
-        await saved_set.refresh()
-        assert saved_set == a_set
+async def test_save_redis_set() -> None:
+    redis_set = RedisSet[str](id="some_set")
+    redis_set.add("test")
+    redis_set.add("ing")
+    await redis_set.save()
 
-    async def test_async_model_list(self) -> None:
-        a_list = RedisList[Foo](id="a_list")
-        await a_list.async_append(Foo("123", 123), cascade=True)
-        await a_list.async_append(Foo("456", 456), cascade=True)
-        assert len(a_list) == 2
-        saved_list = await RedisList[Foo].get("a_list")
-        await saved_list.refresh()
-        assert saved_list == a_list
+    async with connection() as redis:
+        assert {"test", "ing"} == await redis.smembers("redisset:some_set")
 
-    async def test_delete(self) -> None:
-        a_list = RedisList[str](id="a_list")
-        await a_list.async_append("123", cascade=True)
-        await a_list.async_append("456", cascade=True)
-        assert len(a_list) == 2
-        await a_list.delete()
-        self.assertFalse(await RedisList(id="a_list").exists())
+    some_set = await RedisSet[str].get("some_set")
+    await some_set.refresh()
+    assert {"test", "ing"} == some_set.values
 
-    async def test_delete_model(self) -> None:
-        a_list = RedisList[Foo](id="a_list")
-        await a_list.async_append(Foo("123", 123), cascade=True)
-        await a_list.async_append(Foo("456", 456))
-        assert len(a_list) == 2
-        assert await Foo.get("123")
-        await a_list.delete(cascade=True)
-        self.assertFalse(await RedisList[Foo](id="a_list").exists())
-        with self.assertRaises(ModelNotFoundException):
-            await Foo.get("123")
+
+async def test_iteration_set() -> None:
+    redis_set = RedisSet[str](id="some_set")
+    redis_set.add("test")
+    redis_set.add("ing")
+    await redis_set.save()
+    some_set = await RedisSet[str].get("some_set")
+    async for item in some_set:
+        assert item in redis_set
+
+
+async def test_iteration_list() -> None:
+    redis_list = RedisList[str](id="some_list")
+    redis_list.append("test")
+    redis_list.append("ing")
+    await redis_list.save()
+    some_list = await RedisList[str].get("some_list")
+    async for item in some_list:
+        assert item in redis_list
+
+
+async def test_model_iteration(foo: Foo) -> None:
+    model_list = RedisList[Foo]([foo], id="models")
+    await model_list.save(cascade=True)
+    models = await RedisList[Foo].get("models")
+    async for item in models:
+        assert item in model_list
+    assert [foo] == models
+
+
+async def test_async_set() -> None:
+    a_set = RedisSet[str](id="a_set")
+    await a_set.async_add("123")
+    await a_set.async_add("456")
+    assert len(a_set) == 2
+    await a_set.async_discard("456")
+    assert len(a_set) == 1
+    new_set = await RedisSet[str].get("a_set")
+    await new_set.refresh()
+    assert a_set == new_set
+
+
+async def test_async_list() -> None:
+    a_list = RedisList[str](id="a_list")
+    await a_list.async_append("123")
+    await a_list.async_append("456")
+    assert len(a_list) == 2
+    saved_list = await RedisList[str].get("a_list")
+    await saved_list.refresh()
+    assert saved_list == a_list
+
+
+async def test_async_model_set() -> None:
+    a_set = RedisSet[Foo](id="a_set")
+    await a_set.async_add(Foo("123", 123), cascade=True)
+    await a_set.async_add(Foo("456", 456), cascade=True)
+    assert len(a_set) == 2
+    await a_set.async_discard(Foo("456", 456), cascade=True)
+    assert len(a_set) == 1
+    saved_set = await RedisSet[Foo].get("a_set")
+    await saved_set.refresh()
+    assert saved_set == a_set
+
+
+async def test_async_model_list() -> None:
+    a_list = RedisList[Foo](id="a_list")
+    await a_list.async_append(Foo("123", 123), cascade=True)
+    await a_list.async_append(Foo("456", 456), cascade=True)
+    assert len(a_list) == 2
+    saved_list = await RedisList[Foo].get("a_list")
+    await saved_list.refresh()
+    assert saved_list == a_list
+
+
+async def test_delete() -> None:
+    a_list = RedisList[str](id="a_list")
+    await a_list.async_append("123", cascade=True)
+    await a_list.async_append("456", cascade=True)
+    assert len(a_list) == 2
+    await a_list.delete()
+    assert not (await RedisList(id="a_list").exists())
+
+
+async def test_delete_model() -> None:
+    a_list = RedisList[Foo](id="a_list")
+    await a_list.async_append(Foo("123", 123), cascade=True)
+    await a_list.async_append(Foo("456", 456))
+    assert len(a_list) == 2
+    assert await Foo.get("123")
+    await a_list.delete(cascade=True)
+    assert not (await RedisList[Foo](id="a_list").exists())
+    with pytest.raises(ModelNotFoundException):
+        await Foo.get("123")
